4a10c757094f317137bc2a08d31bbb08611e36ec
The-Zealot <96833407+The-Zealot@users.noreply.github.com>
2023-04-14 15:11:27 +0700

    Realized base console widget

--- consolewidget.cpp
+++ consolewidget.cpp
@@ -2,5 +2,45 @@
 
 ConsoleWidget::ConsoleWidget(QWidget *parent) : QWidget(parent)
 {
+    _layout         = new QVBoxLayout(this);
+    _textBrowser    = new QTextBrowser(this);
+    _edit           = new QLineEdit(this);
 
+    this->setLayout(_layout);
+    QFont font;
+    font.setFamily("Consolas");
+    font.setBold(true);
+    font.setPixelSize(12);
+
+    this->setFont(font);
+    _edit->setFont(font);
+    _textBrowser->setFont(font);
+    _textBrowser->setFocusPolicy(Qt::NoFocus);
+
+    _layout->addWidget(_textBrowser);
+    _layout->addWidget(_edit);
+
+    _layout->setSpacing(0);
+    _layout->setContentsMargins(0,0,0,0);
+
+    connect(_edit, &QLineEdit::returnPressed, this, &ConsoleWidget::consoleWrite);
+}
+
+QString ConsoleWidget::interpret(const QString &command)
+{
+    if (command == "cls")
+        _textBrowser->clear();
+
+    return command;                                                                 // TODO
+}
+
+void ConsoleWidget::consoleWrite()
+{
+    _textBrowser->append(interpret(_edit->text()));
+    _edit->clear();
+}
+
+void ConsoleWidget::print(const QString &str)
+{
+    _textBrowser->append(str);
 }
--- consolewidget.h
+++ consolewidget.h
@@ -2,15 +2,25 @@
 #define CONSOLEWIDGET_H
 
 #include <QWidget>
+#include <QTextBrowser>
+#include <QLineEdit>
+#include <QVBoxLayout>
 
 class ConsoleWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit ConsoleWidget(QWidget *parent = nullptr);
+    QString interpret(const QString &command);
 
-signals:
+public slots:
+    void consoleWrite();
+    void print(const QString &str);
 
+private:
+    QTextBrowser* _textBrowser;
+    QLineEdit* _edit;
+    QVBoxLayout* _layout;
 };
 
 #endif // CONSOLEWIDGET_H
